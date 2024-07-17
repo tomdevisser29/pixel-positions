26eb53554d6c201c3c7b1a4bc74aa2bf6a5c5000
Tom de Visser <tomdevisser29@gmail.com>
2024-07-18 00:55:50 +0200

    Created a few components and a beginning for the home page

---
+++ resources/views/components/section-heading.blade.php
@@ -0,0 +1,4 @@
+<div class="inline-flex items-center gap-x-2">
+    <span class="bg-white inline-block size-2"></span>
+    <h3 class="font-bold text-lg">{{ $slot }}</h3>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+    theme: {
+        extend: {
+            black: "#060606",
+        },
+    },
+    plugins: [],
+};
